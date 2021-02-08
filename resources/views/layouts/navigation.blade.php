84db8186d489b387169c7c365a70ecad315a4d70
ranayas <ranayas.dev@gmail.com>
2021-02-07 23:42:13 -0500

    Update header.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,14 +5,12 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard.links.index') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
+                    <a class="font-bold text-xl" href="{{ route('dashboard.links.index') }}">Laralinks</a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard.links.index')" :active="request()->routeIs('dashboard.links.*')">
+                    <x-nav-link :href="route('dashboard.links.index')" :active="request()->routeIs('dashboard.*')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
